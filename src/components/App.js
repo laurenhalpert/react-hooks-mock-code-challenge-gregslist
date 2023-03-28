908ddf2c89fd5fcf9a3b09022e18bbd629643978
Lauren <halpertln@gmail.com>
2023-03-28 14:07:23 -0600

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,40 @@
-import React from "react";
+/*
+App
+|
+|-Header
+    |-Search
+
+|
+|-ListingsContainer
+    |-ListingCard
+
+
+
+
+*/
+import React, {useEffect, useState} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+  const [search, setSearch] = useState("")
+  useEffect(()=>{
+    fetch("http://localhost:6001/listings")
+    .then(r=>r.json())
+    .then(listings=> setListings(listings))
+  })
+
+  function handleDelete(id) {
+    setListings(listings.filter(listing=> listing.id !== id))
+  }
+
+  const displayedListings = listings.filter(listing => listing.description.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header search={search} setSearch={setSearch}/>
+      <ListingsContainer listings={displayedListings} onDel={handleDelete}/>
     </div>
   );
 }
